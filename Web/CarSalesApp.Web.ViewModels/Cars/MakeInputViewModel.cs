6a5d454c3b772a9e07107bd3d3c2146b8b5bd913
Dr <dragunchev1992@gmail.com>
2020-04-06 03:45:11 +0300

    ...

---
+++ Web/CarSalesApp.Web.ViewModels/Cars/MakeInputViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CarSalesApp.Data.Models;
+using CarSalesApp.Services.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarSalesApp.Web.ViewModels.Cars
+{
+    public class MakeInputViewModel : IMapFrom<Make>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
